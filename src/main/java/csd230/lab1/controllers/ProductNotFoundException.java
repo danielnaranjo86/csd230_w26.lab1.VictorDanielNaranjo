1ca0ddec9c74e0f79b8f32a4fe6ad35abf6a860e
Daniel Naranjo <23096864@saultcollege.ca>
2026-02-17 14:18:12 -0500

    Lecture 2.10 in class exercise. Some files deleted from branch Lab 4 to start to working

--- src/main/java/csd230/lab1/controllers/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package csd230.lab1.controllers;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(Long id) {
-        super("Could not find product with ID: " + id);
-    }
-}
